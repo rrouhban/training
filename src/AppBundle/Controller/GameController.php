180265517283038a573bd8721e827e2693bf7b61
Benoit Tavernier <benoit.tavernier@gmail.com>
2018-07-23 17:57:49 +0200

    Exercise 1: new pages

---
+++ src/AppBundle/Controller/GameController.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class GameController
+ *
+ * @Route("/game")
+ */
+class GameController extends AbstractController
+{
+    /**
+     * @Route("", name="game_homepage")
+     */
+    public function homeAction()
+    {
+        $game = [
+            'word' => 'BURGER',
+        ];
+
+        return $this->render('game/home.html.twig', [
+            'game' => $game,
+        ]);
+    }
+
+    /**
+     * @Route("/won", name="game_won")
+     */
+    public function wonAction()
+    {
+        return $this->render('game/won.html.twig');
+    }
+
+    /**
+     * @Route("/hanged", name="game_failed")
+     */
+    public function hangedAction()
+    {
+        return $this->render('game/hanged.html.twig');
+    }
+
+    /**
+     * @Route("/play/{letter}", requirements={"letter"="[A-Z]"}, name="game_play_letter")
+     */
+    public function playLetterAction($letter)
+    {
+        return $this->redirectToRoute('game_homepage');
+    }
+}
